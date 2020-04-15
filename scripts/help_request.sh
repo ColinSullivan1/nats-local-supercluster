8b89119348fb68a5746ff6fb3cbb50c0c9518e95
Colin Sullivan <colin@synadia.com>
2020-04-15 16:54:55 -0600

    use different cluster
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- scripts/help_request.sh
+++ scripts/help_request.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nats-req -s "localhost:4000" --creds ./auth/nkeys/creds/myoperator/myaccount2/myuser.creds help "help me!"
+nats-req -s "localhost:6000" --creds ./auth/nkeys/creds/myoperator/myaccount2/myuser.creds help "help me!"
 
